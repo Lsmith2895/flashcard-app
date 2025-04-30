4b01ee523f5be7a40597175333b1cdf4c70d44b8
Logan Smith <lsmith2895@yahoo.com>
2025-04-30 14:10:49 -0400

    add logic to edit card component

--- flashcard-ui/src/app/edit-cards/edit-cards.component.ts
+++ flashcard-ui/src/app/edit-cards/edit-cards.component.ts
@@ -1,11 +1,83 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FlashDeckService, FlashCard } from '../flash-deck.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-edit-cards',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './edit-cards.component.html',
-  styleUrl: './edit-cards.component.scss'
+  styleUrls: ['./edit-cards.component.scss']
 })
-export class EditCardsComponent {
+export class EditCardsComponent implements OnInit {
+  cards: FlashCard[] = [];
+  isLoading = true;
 
+  newCard: FlashCard = { id: '', front: '', back: '', isFlipped: false };
+  editingCard: FlashCard | null = null;
+
+  constructor(private flashDeckService: FlashDeckService) {}
+
+  ngOnInit(): void {
+    this.loadCards();
+  }
+
+  loadCards() {
+    this.flashDeckService.getAll().subscribe(cards => {
+      this.cards = cards;
+      this.isLoading = false;
+    });
+  }
+
+  createCard() {
+    this.flashDeckService.create(this.newCard).subscribe(() => {
+      this.newCard = { id: '', front: '', back: '', isFlipped: false };
+      this.loadCards();
+    });
+  }
+
+  startEdit(card: FlashCard) {
+    this.editingCard = { ...card };
+  }
+
+  saveEdit() {
+    if (!this.editingCard) return;
+
+    this.flashDeckService.update(this.editingCard).subscribe(() => {
+      this.editingCard = null;
+      this.loadCards();
+    });
+  }
+
+  cancelEdit() {
+    this.editingCard = null;
+  }
+
+  deleteCard(id: string) {
+    this.flashDeckService.delete(id).subscribe(() => {
+      this.loadCards();
+    });
+  }
+
+  // Getter + Setter proxies for safe two-way binding
+  get editFront(): string {
+    return this.editingCard?.front || '';
+  }
+
+  set editFront(value: string) {
+    if (this.editingCard) {
+      this.editingCard.front = value;
+    }
+  }
+
+  get editBack(): string {
+    return this.editingCard?.back || '';
+  }
+
+  set editBack(value: string) {
+    if (this.editingCard) {
+      this.editingCard.back = value;
+    }
+  }
 }

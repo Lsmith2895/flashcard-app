fea1218ad1c6643bdb584842dc3e4972585f67e6
Logan Smith <lsmith2895@yahoo.com>
2025-04-30 11:05:32 -0400

    create edit cards page

---
+++ flashcard-ui/src/app/edit-cards/edit-cards.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-cards',
+  imports: [],
+  templateUrl: './edit-cards.component.html',
+  styleUrl: './edit-cards.component.scss'
+})
+export class EditCardsComponent {
+
+}

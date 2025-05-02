a78556ea5ac8d8bf24e2f5e4808fcd475a71f9fe
Logan Smith <lsmith2895@yahoo.com>
2025-05-02 11:23:53 -0400

    mp new background animation

--- flashcard-ui/src/app/app.component.ts
+++ flashcard-ui/src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import {AnimatedBackgroundComponent} from "../app/animated-background/animated-background.component"
+import { MouseGlowBackgroundComponent } from "./mouse-glow-background/mouse-glow-background.component"
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterModule, AnimatedBackgroundComponent],
+  imports: [RouterModule, MouseGlowBackgroundComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
